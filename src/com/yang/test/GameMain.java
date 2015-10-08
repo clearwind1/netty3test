21c83694f7f111d43ee27662123bce47fff35493
clearwind1 <pior@piordeiMac.local>
2015-10-08 17:49:27 +0800

    netty框架HTTP通信
    使用netty框架做的一个HTTP通信服务端

---
+++ src/com/yang/test/GameMain.java
@@ -0,0 +1,37 @@
+package com.yang.test;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+public class GameMain {
+
+	private final int port;
+	 
+    public GameMain(int port) {
+        this.port = port;
+    }
+ 
+    public void run() {
+        ServerBootstrap bootstrap = new ServerBootstrap(new NioServerSocketChannelFactory(
+                Executors.newCachedThreadPool(), Executors.newCachedThreadPool()));
+        bootstrap.setPipelineFactory(new HttpServerPipelineFactory());
+ 
+        bootstrap.bind(new InetSocketAddress(port));
+        System.out.println("Web socket server started at port " + port + '.');
+        System.out.println("Open your browser and navigate to http://localhost:" + port + '/');
+    }
+ 
+    public static void main(String[] args) {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new GameMain(port).run();
+    }
+        
+}
